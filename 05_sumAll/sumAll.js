7e054ba8b9aaec01d6e4bd28934cc55c6090ab72
CorteSconta <umberto.randon@gmail.com>
2022-08-22 23:25:53 +0200

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(...values) {
+  let sum = 0;
+  for(let value of values) {
+    if (typeof value !== 'number' || Math.min(...values) < 0 ) {
+      return 'ERROR';
+    }
+  }  
+  for(let i = Math.min(...values); i <= Math.max(...values); i++) {
+    sum += i;   
+  } return sum
+}
 
-};
+// const sumAll = function(a, b) {
+//   let sum = 0;
+//   for(let i = a; i <= b; i++) {
+//     sum += i;
+//   } return sum
+// }
 
 // Do not edit below this line
 module.exports = sumAll;
