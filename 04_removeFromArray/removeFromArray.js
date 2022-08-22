d3aa089d216d30e898c275d68f4ebb4b2aa324c3
CorteSconta <umberto.randon@gmail.com>
2022-08-22 17:21:24 +0200

    first 3 exercises done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...values) {
+  let array = arr.indexOf(value);
+  if (array !== -1) {
+    arr.splice(array, 1)
+} return arr
 };
 
 // Do not edit below this line
