b70ba4dffcdb7a30b0d246dbcfcbe6f2402fe3ab
CorteSconta <umberto.randon@gmail.com>
2022-08-23 22:32:56 +0200

    07 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempC) {
+  let cels = (tempC - 32)*(5/9);
+  let roundedC = Math.round(cels*10)/10; 
+  return roundedC;
 };
 
-const ctof = function() {
+const ctof = function(tempF) {
+  let celF = (tempF*(9/5))+32;
+  let roundedF = Math.round(celF*10)/10;
+  return roundedF;
 
 };
 
