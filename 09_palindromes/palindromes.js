6d0a2ffdba127bef000ca255c464e52ca58c746a
CorteSconta <umberto.randon@gmail.com>
2022-08-25 22:30:03 +0200

    09 done with RegEx

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) { 
+  let reverse = word.split('').reverse().join('');
+  word = word.replace(/\W/g, '').toLowerCase();
+  reverse = reverse.replace(/\W/g, '').toLowerCase();
+  if (word === reverse) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
