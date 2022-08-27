352f6de9509d44946d8ff5479c1b6802db6abb76
CorteSconta <umberto.randon@gmail.com>
2022-08-27 15:44:38 +0200

    12 oldest done with reduce.  Need review

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+// const findTheOldest = (arr) => arr.reduce((a, b) => {
+//     if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+//       return a 
+//     } else return b 
+//   }, arr[0])
 
+// ;
+
+// const findTheOldest = (p) => p.reduce((acc, e) => {
+//   const lived = x => x.yearOfDeath - x.yearOfBirth
+//   return lived(e) > lived(acc) ? e : acc
+// }, p[0])
+
+
+const findTheOldest = function(array) { 
+  return array.reduce((a, b) => {
+    const oldest = getAge(a.yearOfBirth, a.yearOfDeath);
+    const current = getAge(b.yearOfBirth, b.yearOfDeath);
+    return oldest < current ? b : a
+  })
 };
+
+const getAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  } return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
