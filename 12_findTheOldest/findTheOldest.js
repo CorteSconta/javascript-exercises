d08143f38bd6f46c1610457fd8127f56654b4778
CorteSconta <umberto.randon@gmail.com>
2022-08-28 22:59:33 +0200

    12 0.4

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-// const findTheOldest = function(array) {
-//   const oldest = array.sort(function(a,b) {
-//     const lastP = a.yearOfDeath - a.yearOfBirth;
-//     const newP = b.yearOfDeath - b.yearOfBirth;
-//     return lastP > newP ? -1 : 1;
-//     return array[0]
-//   }) 
-  
-// };
-
-const findTheOldest = function(array) {
-  let oldest = array.reduce((prev, curr) => {
-    return prev.yearOfDeath - prev.yearOfBirth < curr.yearOfDeath - curr.yearOfBirth
-      ? curr : prev
-  })
-}
-
-=======
-// const findTheOldest = (arr) => arr.reduce((a, b) => {
-//     if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
-//       return a 
-//     } else return b 
-//   }, arr[0])
-
-// ;
-
-// const findTheOldest = (p) => p.reduce((acc, e) => {
-//   const lived = x => x.yearOfDeath - x.yearOfBirth
-//   return lived(e) > lived(acc) ? e : acc
-// }, p[0])
 
 
 const findTheOldest = function(array) { 
@@ -38,7 +7,6 @@ const findTheOldest = function(array) {
     return oldest < current ? b : a
   })
 };
->>>>>>> 352f6de9509d44946d8ff5479c1b6802db6abb76
 
 const getAge = function(birth, death) {
   if (!death) {
