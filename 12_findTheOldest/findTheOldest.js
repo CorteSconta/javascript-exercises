c8876073e3c9efada748bc8a54fdc2b9cb6850b8
CorteSconta <umberto.randon@gmail.com>
2022-08-28 22:54:36 +0200

    12 0.2

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+// const findTheOldest = function(array) {
+//   const oldest = array.sort(function(a,b) {
+//     const lastP = a.yearOfDeath - a.yearOfBirth;
+//     const newP = b.yearOfDeath - b.yearOfBirth;
+//     return lastP > newP ? -1 : 1;
+//     return array[0]
+//   }) 
+  
+// };
 
-};
+const findTheOldest = function(array) {
+  let oldest = array.reduce((prev, curr) => {
+    return prev.yearOfDeath - prev.yearOfBirth < curr.yearOfDeath - curr.yearOfBirth
+      ? curr : prev
+  })
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
